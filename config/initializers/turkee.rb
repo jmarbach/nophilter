48500f3764d415128e111977ca35ad4638c69517
John Marbach <john.marbach@gmail.com>
2015-01-18 01:50:48 -0500

    pushing more configurations

---
+++ config/initializers/turkee.rb
@@ -0,0 +1,9 @@
+# Go to this page https://aws-portal.amazon.com/gp/aws/developer/account/index.html?action=access-key
+# to retrieve your AWS/Mechanical Turk access keys.
+
+AWSACCESSKEYID      = ENV['access_key_id']
+AWSSECRETACCESSKEY  = ENV['secret_access_key']
+
+RTurk::logger.level = Logger::DEBUG
+RTurk.setup(AWSACCESSKEYID, AWSSECRETACCESSKEY, :sandbox => (Rails.env == 'production' ? false : true))
+
